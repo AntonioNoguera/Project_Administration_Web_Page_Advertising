344915cc5ce597377f35e8f58bf7218b436060e7
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-04-20 01:39:25 -0600

    Bases del Query

---
+++ insertQuery.php
@@ -0,0 +1,18 @@
+<?php 
+    $empresa = $_POST['bussiness'];
+    $interesado = $_POST['interested'];
+    $correo = $_POST['mail'];
+    $fecha = $_POST['date'];
+    $hora = $_POST['time']; 
+
+
+    //$conexion = mysqli_connect("localhost","id9406842_root","quesueno2121","id9406842_usuarios")or die("hola");
+
+    //$registros = mysqli_query($conexion, "select user,conta from miembros where user='$conto'")or die("Error de Registros");
+
+
+    //$reg=mysqli_fetch_array($registros);
+
+    echo "como?";
+    
+?>
